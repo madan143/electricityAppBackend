6e9a08746e089f8162f2e97e020192b9960aa68b
Madan Reddy <madanchalla@yahoo.in>
2020-09-11 17:06:43 +0530

    Updated Changes

--- electricityAppBackend/src/main/java/com/madan/electricity/ElectricityApplication.java
+++ electricityAppBackend/src/main/java/com/madan/electricity/ElectricityApplication.java
@@ -3,13 +3,10 @@ package com.madan.electricity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoRepositories("com.madan.repository")
-@ComponentScans({ 
-    @ComponentScan(basePackages = "com.madan.controller")
-})
+@ComponentScan(basePackages = "com.madan.controller")
 @SpringBootApplication
 public class ElectricityApplication {
 	
